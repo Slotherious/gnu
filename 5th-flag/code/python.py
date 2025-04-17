ecfa3301eb705071ebf075420a9793c7619529d4
Slotherious <ahmedalmarhoubi@outlook.com>
2025-04-17 05:20:24 -0400

    Add files via upload

---
+++ 5th-flag/code/python.py
@@ -0,0 +1,17 @@
+import random
+
+def generate_random_number():
+    return random.randint(1, 100)
+
+# Generate some random numbers and calculate something
+num1 = generate_random_number()
+num2 = generate_random_number()
+
+# Some random code to throw you off track
+useless_code = "irrelevant" * 50
+
+# Here's where the flag part is hidden
+# part3: wisdom} 
+# Pay attention to the details in the comments
+
+print(f"Random numbers: {num1}, {num2}")
